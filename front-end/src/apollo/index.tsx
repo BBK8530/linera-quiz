f6ce83795a7e2b156c925f8bc8359a556f7d0f19
BBK <chnqbj@gmail.com>
2025-12-15 23:01:11 +0800

    update env

--- front-end/src/apollo/index.tsx
+++ front-end/src/apollo/index.tsx
@@ -4,20 +4,24 @@ import { createApolloProvider } from '@vue/apollo-option';
 // 配置环境变量类型
 declare global {
   interface ImportMetaEnv {
-    VITE_NODE_URL?: string;
+    VITE_HOST?: string;
     VITE_APP_ID?: string;
     VITE_CHAIN_ID?: string;
     BASE_URL?: string;
+    VITE_PORT?: string;
   }
   
   interface ImportMeta {
     env: ImportMetaEnv;
   }
 }
-
+export function getNodeUrl() {
+  const { VITE_HOST, VITE_PORT, VITE_CHAIN_ID, VITE_APP_ID } = import.meta.env;
+  return `http://${VITE_HOST}:${VITE_PORT}/chains/${VITE_CHAIN_ID}/applications/${VITE_APP_ID}`;
+}
 // 设置GraphQL链接
 const httpLink = createHttpLink({
-  uri: `${import.meta.env.VITE_NODE_URL || 'https://linera.quest/api/chains/687db7197e068a473f2dfff5241f1de21c47c0c3a3a89a5c72c733425d8f73e9/applications/07daa738710e5c6c4e017dfb393289b29a462622f4f0f57a9f2c17dd98ea13f8'}`,
+  uri: getNodeUrl(),
 });
 
 // 创建Apollo客户端
